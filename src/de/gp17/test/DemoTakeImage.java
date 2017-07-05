db39a88e8eeea6b1377bdf23ee5a017822448f46
LaaKii <Kai-Achertal@gmx.de>
2017-07-05 22:05:17 +0200

    TestCommit

--- src/de/gp17/test/DemoTakeImage.java
+++ src/de/gp17/test/DemoTakeImage.java
@@ -8,6 +8,7 @@ public class DemoTakeImage {
 		System.loadLibrary(Core.NATIVE_LIBRARY_NAME);
 		TestTakeImage img = new TestTakeImage();
 		Mat frame = img.takeSnapshot();
+		System.out.println("sallesdfasdf");
 		
 	}
 }
