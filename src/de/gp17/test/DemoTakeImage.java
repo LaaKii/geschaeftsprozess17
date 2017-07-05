7fedca3e05e659d3b7a96de314a97b18a5450017
LaaKii <Kai-Achertal@gmx.de>
2017-07-05 22:08:44 +0200

    Test Rückgängig

--- src/de/gp17/test/DemoTakeImage.java
+++ src/de/gp17/test/DemoTakeImage.java
@@ -8,7 +8,6 @@ public class DemoTakeImage {
 		System.loadLibrary(Core.NATIVE_LIBRARY_NAME);
 		TestTakeImage img = new TestTakeImage();
 		Mat frame = img.takeSnapshot();
-		System.out.println("sallesdfasdf");
 		
 	}
 }
